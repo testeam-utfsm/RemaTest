7286c62e083fd22578015aa6130bf8167234b879
Matías Contreras <spypah@gmail.com>
2023-11-13 02:48:21 -0300

    WIP Conexiones tests

--- tests/api/auctions.test.js
+++ tests/api/auctions.test.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 let conn = async function () {
   return mysql.createConnection({
-    host: 'localhost',
+    host: 'rematest-db',
     user: 'root',
     password: 'root',
     database: 'testing',
