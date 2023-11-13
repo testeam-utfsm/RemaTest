fae2d9111245cd00c2f173e2b0c33511920c3ccc
Matías Contreras <spypah@gmail.com>
2023-11-13 02:25:35 -0300

    WIP Dirección BD en testings

--- tests/api/bids.test.js
+++ tests/api/bids.test.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 let conn = async function () {
   return mysql.createConnection({
-    host: 'rematest-db',
+    host: '0.0.0.0',
     user: 'root',
     password: 'root',
     database: 'testing',
