3adc068d9317b1fffe5e345a97bcaa18f461e6a7
Vicente Lineros Pardo <vlinerospardo@gmail.com>
2023-11-15 23:38:45 -0300

    Update bids.js

--- routes/bids.js
+++ routes/bids.js
@@ -14,7 +14,7 @@ const mysql = require('mysql2/promise');
 
 let conn = async function () {
     return mysql.createConnection({
-      host: 'mysqltestingutfsm',
+      host: 'mysqltestingutfsm.mysql.database.azure.com',
       user: 'testingadmin',
       password: 'LF4DfVLr?+j&}yK',
       database: 'testing',
